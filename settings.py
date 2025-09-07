41d91ba7fe85d49c96f60828f8b7e1dcc8de50d8
kaiizz1 <manzgames5@gmail.com>
2025-09-07 07:53:37 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 # === TELEGRAM OWNER ===
-OWNER_ID = 7756491096
-OWNER_NAME = "JUN NOT DEVS"
-GROUP_CHAT = "junofficial354gc"
-CHANNEL = "junofficial354ch"
-URL_ADMIN = "https://t.me/junofficial354bot"
+OWNER_ID = 6699855714
+OWNER_NAME = "Kaii"
+GROUP_CHAT = "rpkaii"
+CHANNEL = "infokaiidev"
+URL_ADMIN = "https://t.me/KaiiOfficial"
 IP = "152.42.167.204"
 # === BOT INFO ===
-BOT_NAME = "UBOT MMK"
-PP = "https://files.catbox.moe/zzx0s5.jpg"
-BUG_URL = "https://files.catbox.moe/zzx0s5.jpg"
-BOT_TOKEN = "8458392296:AAGt5_OhAN_CViN-CH-gqRRPa-4SIx14DPY"
+BOT_NAME = "Userbot Premium Kaii"
+PP = "https://files.catbox.moe/f5cy5k.jpg"
+BUG_URL = "https://files.catbox.moe/f5cy5k.jpg"
+BOT_TOKEN = "8442754069:AAHZwTuYO_z0_HiJ_8Ggtzh32DpSkw1k2v8"
 # === PTERODACTYL PANEL ===
 DOMAIN = "https://junaoffc.panelmurmer.web.id"
 PLTA = "ptla_ZeiAUwnqme299jhutwvbKYpOS0AWDUORRKJQtG4qbdR"
@@ -18,8 +18,8 @@ LOC = 1
 EGGS = 15
 
 # === TELEGRAM API (my.telegram.org) ===
-API_ID = 23205651
-API_HASH = "5a5d93f4564092db12126729cacf58ac"
+API_ID = 28473241
+API_HASH = "c3df70b867a0e65eed5419cecdd5a5bd"
 
 
 # apa ga bisa ambil api hash ama api id? A N A K  P I N T A R 😊
